5ca6782433901d6456667bbc93fe753a77de37c5
mali <maliselamaj4@gmail.com>
2023-11-18 11:03:27 +0100

    add navbar

---
+++ index.js
@@ -0,0 +1,40 @@
+// document.getElementById("count-el").innerText = 5
+
+
+
+// let count = 0
+
+// console.log(count)
+
+
+// console.log(myAge)
+
+// let myAge = 21
+
+// console.log(myAge)
+
+
+
+// Function is called, the return value will end up in x
+// let x = myFunction(4, 3);
+
+// function myFunction(a,b) {
+// // Function returns the product of a and b
+
+//    return a * b;
+
+// }
+
+
+
+// function toCelsius(fahrenheit) {
+//     return (5/9) * (fahrenheit-32);
+//   }
+  
+
+
+
+// 
+var x;
+x = 10
+alert(x)
